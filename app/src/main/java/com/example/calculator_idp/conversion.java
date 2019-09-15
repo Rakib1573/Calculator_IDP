64e7909d1be57e630e89fcb762e2dc868fca9cfb
Fuadsam <fuad22samin@gmail.com>
2019-09-16 02:36:34 +0600

    commit by Fuad

--- app/src/main/java/com/example/calculator_idp/conversion.java
+++ app/src/main/java/com/example/calculator_idp/conversion.java
@@ -16,6 +16,10 @@ public class conversion extends AppCompatActivity {
     private EditText taka;
     private EditText dollar;
     private Button convert;
+    private EditText kg;
+    private EditText pound;
+    private Button convertpound;
+
     DatabaseReference reference;
     dbCon dbCon;
 
@@ -26,6 +30,9 @@ public class conversion extends AppCompatActivity {
         convert=(Button)findViewById(R.id.btnConvert);
         taka=(EditText)findViewById(R.id.etTaka);
         dollar=(EditText)findViewById(R.id.etDollar);
+        convertpound=(Button)findViewById(R.id.btnConvertpound);
+        kg=(EditText)findViewById(R.id.etkg);
+        pound=(EditText)findViewById(R.id.etpound);
         dbCon=new dbCon();
         reference= FirebaseDatabase.getInstance().getReference().child("dbCon");
     }
@@ -62,6 +69,40 @@ public class conversion extends AppCompatActivity {
             Toast.makeText(conversion.this,"data inserted Successfully",Toast.LENGTH_LONG).show();
         }
         };
+    public void convertpound(View view) {
+
+        if(pound.getText().toString().equals("")) {
+            kg = (EditText) findViewById(R.id.etkg);
+            double kilo = Double.parseDouble(kg.getText().toString());
+            Double Pound = 2.205;
+            double result = kilo * Pound;
+            dbCon.setResult(kg.getText().toString().trim());
+            reference.push().setValue(dbCon);
+            Toast.makeText(conversion.this,"data inserted Successfully",Toast.LENGTH_LONG).show();
+            pound = (EditText) findViewById(R.id.etpound);
+            pound.setText(Double.toString(result) + " pound");
+            dbCon.setResult(pound.getText().toString().trim());
+            reference.push().setValue(dbCon);
+            Toast.makeText(conversion.this,"data inserted Successfully",Toast.LENGTH_LONG).show();
+        }
+
+
+        else if(kg.getText().toString().equals("")){
+            pound = (EditText) findViewById(R.id.etpound);
+            double pound = Double.parseDouble(dollar.getText().toString());
+            Double Kg = 0.454;
+            double result = Kg * pound;
+            dbCon.setResult(dollar.getText().toString().trim());
+            reference.push().setValue(dbCon);
+            Toast.makeText(conversion.this,"data inserted Successfully",Toast.LENGTH_LONG).show();
+            kg = (EditText) findViewById(R.id.etkg);
+            kg.setText(Double.toString(result) + " kg");
+            dbCon.setResult(kg.getText().toString().trim());
+            reference.push().setValue(dbCon);
+            Toast.makeText(conversion.this,"data inserted Successfully",Toast.LENGTH_LONG).show();
+        }
+    };
+
 
     }
 
